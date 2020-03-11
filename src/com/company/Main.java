47fea529fbce3f72d9c1e3f4607427dd32a72ea5
Sanjit Verma <sanjitvermasw@gmail.com>
2020-03-11 16:55:34 -0400

    Lab 6 Final Edition Sort/Shuffle

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,10 +11,18 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         Deck deck= new Deck();
         deck.shuffle();
+        Alert sort = new Alert(AlertType.INFORMATION);
+        sort.setTitle("Sorted Card Output");
+        sort.setHeaderText("Sorted Card Generate");
+        sort.setResizable(true);
+        sort.getDialogPane().setPrefSize(900,340);
+        sort.setContentText(deck.toString());
+        sort.showAndWait();
+
 
         Alert alert = new Alert(AlertType.INFORMATION);
         alert.setTitle("Shuffled Card Output");
-        alert.setHeaderText("Sorted Card Generate");
+        alert.setHeaderText("Shuffled Card Generate");
         alert.setResizable(true);
         alert.getDialogPane().setPrefSize(900,340);
         alert.setContentText(deck.toString());
